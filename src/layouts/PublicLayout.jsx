575e0507090ec5459caa151afa0b099c66be9940
haikalnuril <haikal.nuril23@gmail.com>
2024-11-23 16:56:18 +0700

    fix: tailwind and layout

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const PublicLayout = () => {
-  return (
-    <div>PublicLayout</div>
-  )
-}
+    return (
+      //navbar nanti disini
+        <main className="mx-auto max-w-6xl px-8 py-20 min-h-[80vh]">
+            <Outlet />
+        </main>
 
-export default PublicLayout
+        //footer nanti disini
+    );
+};
+
+export default PublicLayout;
--- src/pages/HomeView.jsx
+++ src/pages/HomeView.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomeView = () => {
   return (
-    <div>HomeView</div>
+    <div className="text-3xl font-bold underline">HomeView</div>
   )
 }
 
