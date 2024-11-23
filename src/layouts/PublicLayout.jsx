d05d739eb626b8993ac4639b927a0a360c992f34
haikalnuril <haikal.nuril23@gmail.com>
2024-11-23 20:57:03 +0700

    feat: skeleton for Form

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const PublicLayout = () => {
     return (
       //navbar nanti disini
-        <main className="mx-auto max-w-6xl px-8 py-20 min-h-[80vh]">
+        <main className="mx-auto min-h-[80vh]">
             <Outlet />
         </main>
 
--- src/pages/HomeView.jsx
+++ src/pages/HomeView.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import FormSkeleton from '../components/skeletons/FormSkeleton'
 
 const HomeView = () => {
   return (
+    <>
+    
     <div className="text-3xl font-bold underline">HomeView</div>
+    <FormSkeleton height="h-96" width="w-96"/>
+    </>
   )
 }
 
