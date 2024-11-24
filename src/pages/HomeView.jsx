c8dab67967c83eaf9efa70ee44cabd010ef0a654
Ananda Darma <anandadarma2612@gmail.com>
2024-11-25 05:13:22 +0800

    feat: adding search button component

--- src/pages/HomeView.jsx
+++ src/pages/HomeView.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import FormSkeleton from "../components/skeletons/FormSkeleton";
 import Navbar from "../components/Navbar/Navbar";
 import HomeCard from "../components/Card/HomeCard";
+import SearchDestination from "../components/Button/Search";
 
 const HomeView = () => {
   return (
@@ -10,7 +11,14 @@ const HomeView = () => {
 
       <section className="container mx-auto">
         <div className="pt-40 text-3xl font-bold underline">HomeView</div>
-        <HomeCard />
+
+        <div>
+          <h2 className="text-base font-bold px-[200px] mb-4">
+            Destinasi Favorit
+          </h2>
+          <SearchDestination />
+          <HomeCard />
+        </div>
       </section>
 
       {/* <FormSkeleton height="h-96" width="w-96"/> */}
