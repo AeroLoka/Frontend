1426b9cd6ba599b22d4615a54ded60f0e6ce2011
xkylz1 <fauzansaputra741@gmail.com>
2024-11-26 21:48:31 +0800

    feat: add payment status  route

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,44 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import PublicLayout from "./layouts/PublicLayout";
 import HomeView from "./pages/HomeView";
 import OrderPage from "./pages/OrderPageView";
+import PaymentStatusPage from "./pages/PaymentStatusPage";
 
 import LoginView from "./pages/auth/LoginView";
 import RegisterView from "./pages/auth/RegisterView";
 
 //loader
 
-
-
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <PublicLayout />,
-        children: [
-            {
-                index: true,
-                element: <HomeView />,
-            },
-            {
-              path: '/order-page',
-              element: <OrderPage />,
-            }
-        ],
-    },
-    {
-      path: "/login",
-      element: <LoginView/>
-    },
-    {
-      path: "/register",
-      element: <RegisterView/>
-    }
+  {
+    path: "/",
+    element: <PublicLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomeView />,
+      },
+      {
+        path: "/order-page",
+        element: <OrderPage />,
+      },
+      {
+        path: `/payment-status`,
+        element: <PaymentStatusPage />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LoginView />,
+  },
+  {
+    path: "/register",
+    element: <RegisterView />,
+  },
 ]);
 
 function App() {
-    return(
-      <RouterProvider router={router} />
-    )
+  return <RouterProvider router={router} />;
 }
 
 export default App;
