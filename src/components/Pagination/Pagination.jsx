e1731b9307c30e0227b5df4d174c9729252b3cea
Ananda Darma <anandadarma2612@gmail.com>
2024-12-02 09:01:55 +0800

    feat: adding pagination for card content at home page

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,43 @@
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa6";
+
+const Pagination = ({ currPage, totalPages, onPageChange }) => {
+  const prev = () => {
+    if (currPage > 1) {
+      onPageChange(currPage - 1);
+    }
+  };
+
+  const next = () => {
+    if (currPage < totalPages) {
+      onPageChange(page + 1);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center gap-1 py-5 mt-7">
+      <button onClick={prev} disabled={currPage === 1} className="mx-3">
+        <FaAngleLeft />
+      </button>
+      {Array.from({ length: totalPages }, (_, index) => (
+        <div className="">
+          <button
+            key={index}
+            onClick={() => onPageChange(index + 1)}
+            className={`px-3 py-1 text-sm font-medium rounded-md ${
+              currPage === index + 1
+                ? "bg-[#7126B5] text-white"
+                : "bg-[#E2D4F0] text-[#7126B5] hover:bg-[#7126B5] hover:text-white"
+            }`}
+          >
+            {index + 1}
+          </button>
+        </div>
+      ))}
+      <button onClick={next} disabled={currPage === 1} className="mx-3">
+        <FaAngleRight />
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
--- src/pages/HomeView.jsx
+++ src/pages/HomeView.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import FormSkeleton from "../components/skeletons/FormSkeleton";
 import Navbar from "../components/Navbar/Navbar";
 import HomeCard from "../components/Card/HomeCard";
 import SearchDestination from "../components/Button/SearchButton";
 import DiscountBanner from "../components/Banner/Banner";
 import SearchFlight from "../components/Flight/SearchFlight";
+import Pagination from "../components/Pagination/Pagination";
 
 const HomeView = () => {
+
+  const limit = 5;
+  const [page, setPage] = useState(1);
+  const offset = (page - 1) * limit;
+
+  // const totalPages = Math.ceil(data.length / limit);
+  // const currPageData = data.slice(offset, offset + limit);
+
   return (
     <>
       <Navbar />
@@ -20,13 +29,19 @@ const HomeView = () => {
         </div>
       </section>
 
-      <section className="container relative mx-auto pt-72 mb-20">
+      <section className="container relative mx-auto pt-72">
         <div>
           <h2 className="text-base font-bold px-[200px] mb-4">
             Destinasi Favorit
           </h2>
           <SearchDestination />
           <HomeCard />
+          
+          <Pagination
+            // currPage={page}
+            // totalPages={totalPages}
+            // onPageChange={(newPage) => setPage(newPage)}
+          />
         </div>
       </section>
 
