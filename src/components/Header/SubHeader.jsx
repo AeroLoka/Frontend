9305566e8d496b7492b9b307e82004d714ec8579
haikalnuril <haikal.nuril23@gmail.com>
2024-12-03 15:52:29 +0700

    feat: detail profile and account settings

---
+++ src/components/Header/SubHeader.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { FaArrowLeft } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
+const SubHeader = (props) => {
+    const navigate = useNavigate();
+    const { label, children } = props;
+    const handleBeranda = () => {
+        navigate('/');
+    }
+    return (
+        <div className="shadow-md">
+            <div className="max-w-7xl pl-4 mx-auto mt-32 font-bold text-xl flex">
+                <p>{label}</p>
+            </div>
+            <div className="max-w-7xl pl-4 mx-auto py-3">
+                <button className="flex items-center space-x-4 pl-4 text-left text-white w-full bg-red-500 p-2 rounded-lg" onClick={handleBeranda}>
+                    <FaArrowLeft />
+                    <span>Beranda</span>
+                </button>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default SubHeader;
--- src/pages/ProfileView.jsx
+++ src/pages/ProfileView.jsx
@@ -1,45 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
-import SubHeader from "../components/Navbar/SubHeader";
+import SubHeader from "../components/Header/SubHeader";
 import { LuPenLine } from "react-icons/lu";
 import { CiSettings } from "react-icons/ci";
 import { RxExit } from "react-icons/rx";
 import { FaArrowLeft } from "react-icons/fa";
+import DetailProfile from "../components/Profile/DetailProfile";
+import AccountSettings from "../components/Profile/AccountSettings";
 
 const ProfileView = () => {
+    const [activeView, setActiveView] = useState("profile");
     return (
         <>
             <Navbar />
             <SubHeader label="Akun">
-                <button  className="flex items-center space-x-4 pl-4 text-left text-white w-full bg-red-500 p-2 rounded-lg">
-                <FaArrowLeft />
-                <span>Beranda</span>
-                </button>
             </SubHeader>
-            <div className="max-w-7xl mx-auto p-4 bg-black">
+            <div className="max-w-7xl mx-auto p-4">
                 <div className="grid md:grid-cols-[35%_64%] gap-4">
                     <div className="space-y-6">
                         <div className="bg-white rounded-lg border p-6 flex flex-col">
-                            <button className="flex items-center space-x-2 py-2 text-left w-full border-b border-gray-300 font-bold">
+                            <button
+                                onClick={() => setActiveView("profile")}
+                                className={`flex items-center space-x-2 py-2 text-left w-full border-b border-gray-300 ${
+                                    activeView === "profile"
+                                        ? "font-bold text-purple-500"
+                                        : ""
+                                }`}
+                            >
                                 <LuPenLine />
                                 <span>Ubah Profile</span>
                             </button>
-                            <button className="flex items-center space-x-2 py-2 text-left w-full border-b border-gray-300 font-bold">
+                            <button
+                                onClick={() => setActiveView("settings")}
+                                className={`flex items-center space-x-2 py-2 text-left w-full border-b border-gray-300 ${
+                                    activeView === "settings"
+                                        ? "font-bold text-purple-500"
+                                        : ""
+                                }`}
+                            >
                                 <CiSettings />
                                 <span>Account Settings</span>
                             </button>
-                            <button className="flex items-center space-x-2 py-2 text-left w-full border-b border-gray-300 font-bold">
+                            <button
+                                className={`flex items-center space-x-2 py-2 text-left w-full border-b border-gray-300`}
+                            >
                                 <RxExit />
-                                <span>
-                                    Logout
-                                </span>
+                                <span>Logout</span>
                             </button>
-                            <p className="text-center text-sm font-extralight">version 1.1.0</p>
+                            <p className="text-center text-sm font-extralight">
+                                version 1.1.0
+                            </p>
                         </div>
                     </div>
                     <div>
-                        <div className="bg-slate-500 rounded-lg p-6">
-                            
+                        <div className="border rounded-lg p-6">
+                            {activeView === "profile" ? (
+                                <DetailProfile />
+                            ) : (
+                                <AccountSettings />
+                            )}
                         </div>
                     </div>
                 </div>
