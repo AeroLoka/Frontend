be297e339d7d4aa09fe96a93ccbb14e625a7ff69
Ananda Darma <anandadarma2612@gmail.com>
2024-12-03 21:36:40 +0800

    feat: login form and login view pages

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,50 +1,85 @@
+import { FormProvider, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import InputForm from "../form/InputForm";
+
 const Login = () => {
+  const navigate = useNavigate();
+  const methods = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <h1>Daftar</h1>
-      <form action="">
-        <label htmlFor="" className="flex flex-col w-full">
-          Nama
-        </label>
-        <input
-          type="text"
-          placeholder="Nama Lengkap"
-          className="flex items-center mb-[16px] w-[453px] h-[48px] rounded-[16px] border-solid border-2 border-[#D0D0D0]"
-        />
-        <label for="email" className="text-[12px]">
-          Email
-        </label>
-        <input
-          type="email"
-          placeholder="Contoh: johndee@gmail.com"
-          className="flex items-center mb-[16px] w-[453px] h-[48px] rounded-[16px] border-solid border-2 border-[#D0D0D0]"
-        />
+    <div className="flex flex-col items-center justify-center">
+      <div>
+        <div>
+          <h1 className="text-xl font-bold mb-4">Masuk</h1>
+        </div>
+        <div>
+          <FormProvider {...methods}>
+            <form action="" onSubmit={methods.handleSubmit(onSubmit)}>
+              <InputForm
+                name="emailOrPhone"
+                type="text"
+                label="Email/No Telepon"
+                placeholder="Contoh: johndoe@gmail.com"
+                validation={{
+                  required: "Email atau nomot telepon wajib diisi",
+                  pattern: {
+                    value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                    message: "Format email tidak valid",
+                  },
+                }}
+              />
 
-        <label for="phone" className="text-[12px]">
-          Nomor Telepon
-        </label>
-        <input
-          type="text"
-          placeholder="+62"
-          className="flex items-center mb-[16px] w-[453px] h-[48px] rounded-[16px] border-solid border-2 border-[#D0D0D0]"
-        />
+              <div className="flex justify-between items-center mb-1">
+                <label htmlFor="password" className="text-sm">
+                  Password
+                </label>
+                <span className="text-[#7126B5] font-medium text-sm">
+                  <a href="#">Lupa Kata Sandi</a>
+                </span>
+              </div>
 
-        <label for="password" className="text-[12px]">
-          Buat Password
-        </label>
-        <input
-          type="password"
-          placeholder="Buat Password"
-          className="flex items-center mb-[24px] w-[453px] h-[48px] rounded-[16px] border-solid border-2 border-[#D0D0D0]"
-        />
+              <InputForm
+                name="password"
+                type="password"
+                placeholder="Masukkan password"
+                validation={{
+                  required: "Password wajib diisi",
+                  pattern: {
+                    value: 0,
+                    message: "Password minimal 6 karakter",
+                  },
+                }}
+              />
 
-        <button
-          type="submit"
-          className="w-[453px] h-[48px] bg-[#7126B5] text-white rounded-[16px] mb-[48px]"
-        >
-          Daftar
-        </button>
-      </form>
+              <button
+                type="submit"
+                className="w-[453px] h-[48px] pl-3 bg-[#7126B5] text-white rounded-[16px] mb-[48px]"
+              >
+                Masuk
+              </button>
+            </form>
+          </FormProvider>
+
+          <div className="text-center">
+            <p className="">
+              Belum punya akun?
+              <a href="">
+                <span
+                  onClick={() => navigate("/register")}
+                  className="text-[#7126B5] font-bold"
+                >
+                  {" "}
+                  Daftar di sini
+                </span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/auth/LoginView.jsx
+++ src/pages/auth/LoginView.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import Login from "../../components/Auth/Login";
-import authBanner from "../../assets/images/auth-banner.png"
+import authBanner from "../../assets/images/auth-banner.png";
 
 const LoginView = () => {
   return (
-    
-      <div className="grid grid-cols-1 lg:grid-cols-2 h-screen">
-        <div className="">
-          <img src={authBanner} alt="" className="h-full" />
-        </div>
-        <Login />
+    <div className="grid grid-cols-1 h-screen lg:grid-cols-2">
+      <div className="hidden md:block">
+        <img src={authBanner} alt="" className="" />
       </div>
-    
+      <Login />
+    </div>
   );
 };
 
