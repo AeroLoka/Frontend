0b0e23a363b33b07332b3d8349cb01b7a2869d20
Nitaa1904 <nitafitrotul1904@gmail.com>
2024-12-05 21:13:51 +0700

    feat: add route for OrderHistory component

--- src/App.jsx
+++ src/App.jsx
@@ -93,3 +93,79 @@ function App() {
 }
 
 export default App;
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//view
+import PublicLayout from "./layouts/PublicLayout";
+import HomeView from "./pages/HomeView";
+import DetailTicket from "./pages/DetailTicketView";
+import OrderPage from "./pages/OrderPageView";
+import PaymentView from "./pages/PaymentView";
+import PaymentStatusPage from "./pages/PaymentStatusPage";
+
+import LoginView from "./pages/auth/LoginView";
+import RegisterView from "./pages/auth/RegisterView";
+import ProfileView from "./pages/ProfileView";
+
+//loader
+
+//storage
+import { store } from "./store";
+
+import ButtonChange from "./components/Button/ButtonChange";
+import OrderHistory from "./pages/OrderHistoryView";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PublicLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomeView />,
+      },
+      {
+        path: "profile",
+        element: <ProfileView />,
+      },
+      {
+        path: "/detail-ticket",
+        element: <DetailTicket />,
+      },
+      {
+        path: "/search",
+        element: <ButtonChange />,
+      },
+      {
+        path: "/order-page",
+        element: <OrderPage />,
+      },
+      {
+        path: "/payment",
+        element: <PaymentView />,
+      },
+      {
+        path: `/payment-status`,
+        element: <PaymentStatusPage />,
+      },
+      {
+        path: `/order-history`,
+        element: <OrderHistory />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LoginView />,
+  },
+  {
+    path: "/register",
+    element: <RegisterView />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
