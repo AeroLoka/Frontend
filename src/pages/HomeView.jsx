875e5aa5cd9299b5fd0d37d74b66684eb30551b0
xkylz1 <fauzansaputra741@gmail.com>
2024-12-09 22:05:59 +0800

    feat: implement conditional rendering for navbar when user logged in or not

--- src/pages/HomeView.jsx
+++ src/pages/HomeView.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import FormSkeleton from "../components/skeletons/FormSkeleton";
 import Navbar from "../components/Navbar/Navbar";
+import LoggedInNavbar from "../components/Navbar/LoggedInNavbar";
 import HomeCard from "../components/Card/HomeCard";
 import SearchDestination from "../components/Button/SearchButton";
 import DiscountBanner from "../components/Banner/Banner";
 import SearchFlight from "../components/Flight/SearchFlight";
 
 const HomeView = () => {
+  const { user } = useSelector((state) => state.userState);
   return (
     <>
-      <Navbar />
-
+      {user ? <LoggedInNavbar /> : <Navbar />}
       <section>
         <div className="relative">
           <DiscountBanner />
@@ -19,7 +21,6 @@ const HomeView = () => {
           <SearchFlight />
         </div>
       </section>
-
       <section className="container relative mx-auto pt-72 mb-20">
         <div>
           <h2 className="text-base font-bold px-[200px] mb-4 mt-8">
@@ -29,7 +30,6 @@ const HomeView = () => {
           <HomeCard />
         </div>
       </section>
-
       {/* <FormSkeleton height="h-96" width="w-96"/> */}
     </>
   );
