51ec0a86a9997a7509c3b41f2d643a3e300638d0
xkylz1 <fauzansaputra741@gmail.com>
2024-12-09 23:53:31 +0800

    feat: implement conditional rendering

--- src/components/Header/SubHeader.jsx
+++ src/components/Header/SubHeader.jsx
@@ -5,22 +5,43 @@ import { useNavigate } from "react-router-dom";
 const SubHeader = (props) => {
   const navigate = useNavigate();
   const { label, children } = props;
+
   const handleBeranda = () => {
     navigate("/");
   };
+
   return (
     <div className="shadow-md">
-      <div className="max-w-7xl pl-4 mx-auto mt-32 font-bold text-xl flex">
+      <div className="max-w-7xl pl-4 mx-auto mt-32 font-bold text-xl flex items-center">
         <p>{label}</p>
       </div>
+
+      {/* Content Section */}
       <div className="max-w-7xl pl-4 mx-auto py-3 px-4">
-        <button
-          className="flex items-center space-x-4 pl-4 text-left text-white w-full bg-purple-500 p-2 rounded-lg"
-          onClick={handleBeranda}
-        >
-          <FaArrowLeft />
-          <span>Beranda</span>
-        </button>
+        <div className="flex items-center space-x-2">
+          {/* Beranda Button */}
+          <button
+            className="flex-1 flex items-center space-x-4 text-white bg-purple-500 p-2 rounded-lg"
+            onClick={handleBeranda}
+          >
+            <FaArrowLeft />
+            <span>Beranda</span>
+          </button>
+          {label === "Notifikasi" && (
+            <div className="flex items-center space-x-2">
+              <img
+                src="/icons/filter_icon.svg"
+                alt="Filter Icon"
+                className=" h-8"
+              />
+              <img
+                src="/icons/search_notif_icon.svg"
+                alt="Search Icon"
+                className="w-6 h-6"
+              />
+            </div>
+          )}
+        </div>
         {children}
       </div>
     </div>
