8d4f80eb9a49b053761d2ac197d0289677bec478
xkylz1 <fauzansaputra741@gmail.com>
2024-12-10 00:59:47 +0800

    feat: add red and blue icon

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -8,18 +8,25 @@ const Notification = ({ notification }) => {
           <img src="icons/notification.svg" alt="" className="w-6 h-6" />
           <p className="text-[#8A8A8A] text-[14px]">{notification.type}</p>
         </div>
-        <p className="text-[#8A8A8A] text-[14px]">
-          {new Date(notification.date).toLocaleDateString("id-ID", {
-            day: "2-digit",
-            month: "long",
-          })}
-          ,{" "}
-          {new Date(notification.date).toLocaleTimeString("id-ID", {
-            hour: "2-digit",
-            minute: "2-digit",
-            hour12: false,
-          })}
-        </p>
+        <div className="flex gap-4">
+          <p className="text-[#8A8A8A] text-[14px]">
+            {new Date(notification.date).toLocaleDateString("id-ID", {
+              day: "2-digit",
+              month: "long",
+            })}
+            ,{" "}
+            {new Date(notification.date).toLocaleTimeString("id-ID", {
+              hour: "2-digit",
+              minute: "2-digit",
+              hour12: false,
+            })}
+          </p>
+          <span
+            className={`mt-1.5 ml-2 w-2 h-2 rounded-full ${
+              notification.type === "Promosi" ? "bg-green-500" : "bg-red-500"
+            }`}
+          ></span>{" "}
+        </div>
       </div>
 
       <div className="ml-10">
