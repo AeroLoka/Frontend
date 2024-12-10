c350949c4f5e73f0e056d41b9b595d8f12ad842d
xkylz1 <fauzansaputra741@gmail.com>
2024-12-10 13:11:07 +0800

    fix: file

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import VerifyEmailView from "./pages/auth/VerifyEmailView";
 import ResetPasswordView from "./pages/auth/ResetPasswordView";
 import OTPView from "./pages/auth/OTPView";
 import NotFound from "./pages/NotFound";
+import OrderHistory from "./pages/OrderHistoryView";
 
 const router = createBrowserRouter([
   {
@@ -60,6 +61,10 @@ const router = createBrowserRouter([
         path: `/notifications`,
         element: <NotificationView />,
       },
+      {
+        path: `/order-history`,
+        element: <OrderHistory />,
+      },
     ],
   },
   {
@@ -85,82 +90,6 @@ const router = createBrowserRouter([
   {
     path: "/*",
     element: <NotFound />,
-  },
-]);
-
-function App() {
-  return <RouterProvider router={router} />;
-}
-
-export default App;
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-//view
-import PublicLayout from "./layouts/PublicLayout";
-import HomeView from "./pages/HomeView";
-import DetailTicket from "./pages/DetailTicketView";
-import OrderPage from "./pages/OrderPageView";
-import PaymentView from "./pages/PaymentView";
-import PaymentStatusPage from "./pages/PaymentStatusPage";
-
-import LoginView from "./pages/auth/LoginView";
-import RegisterView from "./pages/auth/RegisterView";
-import ProfileView from "./pages/ProfileView";
-
-//loader
-
-//storage
-import { store } from "./store";
-
-import ButtonChange from "./components/Button/ButtonChange";
-import OrderHistory from "./pages/OrderHistoryView";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <PublicLayout />,
-    children: [
-      {
-        index: true,
-        element: <HomeView />,
-      },
-      {
-        path: "profile",
-        element: <ProfileView />,
-      },
-      {
-        path: "/detail-ticket",
-        element: <DetailTicket />,
-      },
-      {
-        path: "/search",
-        element: <ButtonChange />,
-      },
-      {
-        path: "/order-page",
-        element: <OrderPage />,
-      },
-      {
-        path: "/payment",
-        element: <PaymentView />,
-      },
-      {
-        path: `/payment-status`,
-        element: <PaymentStatusPage />,
-      },
-      {
-        path: `/order-history`,
-        element: <OrderHistory />,
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <LoginView />,
-  },
-  {
-    path: "/register",
-    element: <RegisterView />,
   },
 ]);
 
