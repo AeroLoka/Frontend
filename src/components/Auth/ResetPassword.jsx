0f94b83438bc55dc6ebc6a9bd330130fb280138e
haikalnuril <haikal.nuril23@gmail.com>
2024-12-11 17:10:19 +0700

    fix: password input hide and show

--- src/components/Auth/ResetPassword.jsx
+++ src/components/Auth/ResetPassword.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm, FormProvider } from "react-hook-form";
 import InputForm from "../form/InputForm";
 import { toast } from "react-toastify";
 import { resetPassword } from "../../services/auth.service";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 const ResetPassword = () => {
     const navigate = useNavigate();
     const [searchParams] = useSearchParams();
     const token = searchParams.get("token");
 
+    const [showPassword, setShowPassword] = useState(false);
     const methods = useForm();
     const { handleSubmit } = methods;
 
@@ -19,7 +21,7 @@ const ResetPassword = () => {
                 password: data.password,
                 confirm_password: data.confirm_password,
             };
-            const response = await resetPassword( token, resetData);
+            const response = await resetPassword(token, resetData);
             toast.success(response.message);
             navigate("/login");
         } catch (error) {
@@ -30,26 +32,60 @@ const ResetPassword = () => {
         <div className="">
             <FormProvider {...methods}>
                 <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-                    <InputForm
-                        name="password"
-                        label="Password"
-                        placeholder="Enter your password"
-                        type="password"
-                        validation={{
-                            required: "Password is required",
-                        }}
-                        className="placeholder:text-xs placeholder:lg:text-md"
-                    />
-                    <InputForm
-                        name="confirm_password"
-                        label="Confirm Password"
-                        placeholder="Enter your password"
-                        type="password"
-                        validation={{
-                            required: "Password is required",
-                        }}
-                        className="placeholder:text-xs placeholder:lg:text-md"
-                    />
+                    <div className="relative">
+                        <InputForm
+                            name="password"
+                            label="Buat Password"
+                            placeholder="Buat Password"
+                            type={showPassword ? "text" : "password"}
+                            validation={{
+                                required: "Password wajib diisi",
+                                minLength: {
+                                    value: 6,
+                                    message: "Password minimal 6 karakter",
+                                },
+                            }}
+                            className="placeholder:text-xs placeholder:lg:text-md"
+                        />
+                        <button
+                            type="button"
+                            onClick={() => setShowPassword(!showPassword)}
+                            className="absolute right-3 top-[36px] text-gray-500 hover:text-gray-700 focus:outline-none"
+                        >
+                            {showPassword ? (
+                                <FaRegEyeSlash className="h-5 w-5" />
+                            ) : (
+                                <FaRegEye className="h-5 w-5" />
+                            )}
+                        </button>
+                    </div>
+                    <div className="relative">
+                        <InputForm
+                            name="confirm_password"
+                            label="konfirmasi Password"
+                            placeholder="Ulangi Password"
+                            type={showPassword ? "text" : "password"}
+                            validation={{
+                                required: "Password wajib diisi",
+                                minLength: {
+                                    value: 6,
+                                    message: "Password minimal 6 karakter",
+                                },
+                            }}
+                            className="placeholder:text-xs placeholder:lg:text-md"
+                        />
+                        <button
+                            type="button"
+                            onClick={() => setShowPassword(!showPassword)}
+                            className="absolute right-3 top-[36px] text-gray-500 hover:text-gray-700 focus:outline-none"
+                        >
+                            {showPassword ? (
+                                <FaRegEyeSlash className="h-5 w-5" />
+                            ) : (
+                                <FaRegEye className="h-5 w-5" />
+                            )}
+                        </button>
+                    </div>
                     <button
                         type="submit"
                         className="w-full bg-[#7126B5] text-white py-3 px-4 rounded-lg hover:bg-[#531d85] transition-colors"
