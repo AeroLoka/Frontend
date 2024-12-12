8ba5af90aea614c4f065c09b0d5f059cffbd0054
Ananda Darma <anandadarma2612@gmail.com>
2024-12-12 12:24:16 +0800

    fix: passenger modal handle close and button color

--- src/components/Modals/PassengerModal.jsx
+++ src/components/Modals/PassengerModal.jsx
@@ -1,3 +1,6 @@
+import React, { useEffect, useRef } from "react";
+import { FaMinus, FaPlus } from "react-icons/fa";
+
 const PassengerModal = ({ isOpen, onClose, passengers, onPassengerChange }) => {
   const passengerDetails = [
     {
@@ -33,39 +36,64 @@ const PassengerModal = ({ isOpen, onClose, passengers, onPassengerChange }) => {
     }
   };
 
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      const handleClickOutside = (e) => {
+        if (modalRef.current && !modalRef.current.contains(e.target)) {
+          onClose();
+        }
+      };
+
+      document.addEventListener("mousedown", handleClickOutside);
+      return () =>
+        document.removeEventListener("mousedown", handleClickOutside);
+    }
+  }, [isOpen, onClose]);
+
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 z-20 bg-black bg-opacity-70 flex justify-center items-center">
-      <div className="w-[400px] h-[305px] bg-white p-6 rounded-lg shadow-xl">
+      <div
+        ref={modalRef}
+        className="w-[90%] min-h-[305px] bg-white p-6 rounded-lg shadow-xl lg:w-[30%]"
+      >
         <button className="w-full pb-5 flex justify-end">
-          <img onClick={onClose} src="/icons/fi_close.svg" alt="" />
+          <img onClick={onClose} src="/icons/fi_close.svg" alt="Close Button" />
         </button>
         <div className="space-y-4">
           {passengerDetails.map(({ type, description, iconPath }) => (
             <div key={type} className="flex items-center justify-between">
               <div className="flex items-center">
-                <img src={iconPath} alt="" />
+                <img src={iconPath} alt="Passenger Icon" />
                 <div className="ml-2">
                   <h3 className="capitalize font-bold">{type}</h3>
                   <p className="text-sm">{description}</p>
                 </div>
               </div>
-              <div className="flex items-center space-x-1">
+              <div className="flex items-center space-x-1.5">
                 <button
                   onClick={() => handleDecrement(type)}
-                  className="w-10 h-10 bg-white rounded-md flex justify-center items-center border border-[#7126B5]"
+                  className="w-10 h-10 bg-white rounded-md flex justify-center items-center border border-[#7126B5] hover:border-2"
                 >
-                  <img src="/icons/fi_decrement.svg" alt="" />
+                  <FaMinus className="text-[#D0D0D0] hover:text-[#7126B5]" />
                 </button>
-                <span className="w-14 h-10 text-center flex items-center justify-center rounded-md border border-[#D0D0D0]">
+                <span
+                  className={`w-14 h-10 text-center flex items-center justify-center rounded-md border ${
+                    passengers[type] > 0
+                      ? "border-[#7126B5] text-[#7126B5] border-2 font-semibold"
+                      : "border-[#D0D0D0] text-[#D0D0D0]"
+                  }`}
+                >
                   {passengers[type]}
                 </span>
                 <button
                   onClick={() => handleIncrement(type)}
-                  className="w-10 h-10 bg-white rounded-md flex justify-center items-center border border-[#7126B5]"
+                  className="w-10 h-10 bg-white rounded-md flex justify-center items-center border border-[#7126B5] hover:border-2"
                 >
-                  <img src="/icons/fi_increment.svg" alt="" />
+                  <FaPlus className="text-[#D0D0D0] hover:text-[#7126B5]" />
                 </button>
               </div>
             </div>
