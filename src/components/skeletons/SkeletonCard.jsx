6d13f50fb1d619d1b840ba11f700645cab973fbe
Ananda Darma <anandadarma2612@gmail.com>
2024-12-17 21:34:57 +0800

    fix: skeleton card color

--- src/components/skeletons/SkeletonCard.jsx
+++ src/components/skeletons/SkeletonCard.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const SkeletonCard = () => {
   return (
     <>
-      <div className="relative flex flex-col mb-1 items-start justify-center p-3 bg-slate-300 rounded-lg shadow-lg">
+      <div className="relative flex flex-col mb-1 items-start justify-center p-3 bg-slate-200 rounded-lg shadow-lg">
         <div className="relative mb-3 w-full h-auto">
-          <div className="w-full min-h-44 bg-slate-400 rounded-lg md:min-h-36"></div>
-          <div className="absolute top-0 right-0 w-[50%] h-[25%] text-base font-medium rounded-tl-xl rounded-bl-xl bg-slate-500 text-center text-white flex items-center justify-center lg:text-xs"></div>
+          <div className="w-full min-h-44 bg-slate-300 rounded-lg md:min-h-36"></div>
+          <div className="absolute top-0 right-0 w-[50%] h-[25%] text-base font-medium rounded-tl-xl rounded-bl-xl bg-slate-400 text-center text-white flex items-center justify-center lg:text-xs"></div>
         </div>
         <div className="flex flex-col gap-1 text-left w-full md:gap-1.5">
           <p className="flex flex-wrap items-center max-w-52 min-h-4 bg-slate-400"></p>
